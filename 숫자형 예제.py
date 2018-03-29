81f1d5f2539cca5a83d5ef3ae5d3794b6a262688
cwlee1 <cwlee0511@naver.com>
2018-03-29 18:54:01 +0900

    practice

---
+++ 숫자형 예제.py
@@ -0,0 +1,34 @@
+a = 123
+print(a)
+a = -178
+print(a)
+a = 0
+print(a)
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+a = 4.2E10
+print(a)
+a = 4.24e-10
+print(a)
+a = 0o177
+print(a)
+a = 0x8ff
+print(a)
+b = 0xABC
+print(b)
+a = 3
+b = 4
+print(a+b)
+print(a*b)
+print(a/b)
+a = 3
+b = 4
+print(a**b)
+print(7 % 3)
+print(3 % 7)
+print(7 / 4)
+print(7 // 4)
+print(-7 / 4)
+print(-7 // 4)
