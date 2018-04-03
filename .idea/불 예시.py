9ca8d78f3233f8ffb58248ab2c8febc4d08cecc5
cwlee1 <cwlee0511@naver.com>
2018-04-03 16:56:13 +0900

    practice python

---
+++ .idea/불 예시.py
@@ -0,0 +1,28 @@
+a = True
+b = False
+
+1 == 1
+2 > 1
+2 < 1
+
+a =[1, 2, 3, 4]
+while a:
+    print(a.pop())
+
+if []:
+    print("참")
+else:
+    print("거짓")
+
+
+if [1, 2, 3]:
+    print("참")
+else:
+    print("거짓")
+
+bool('Python')
+bool('')
+bool([1,2,3])
+bool([])
+bool(0)
+bool(3)
